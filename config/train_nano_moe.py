f0004077373e6ab0658c750ffccdb253b101efd2
cameronwolfe <cameronwolfe@netflix.com>
2025-03-09 08:23:17 -0500

    rename nano-moe training config

--- config/train_nano_moe.py
+++ config/train_nano_moe.py
@@ -2,16 +2,31 @@ import time
 
 # config for training GPT-2 (124M) baseline model (one expert) on two RTX 3090 GPUs
 # launch as the following (e.g. in a screen session) and wait ~5 days:
-# $ torchrun --standalone --nproc_per_node=2 train.py config/train_gpt2_moe_baseline.py
+# $ torchrun --standalone --nproc_per_node=2 train.py config/train_nano_moe.py
 
 wandb_log = True
-init_from = 'resume' #'scratch'
+init_from = 'scratch'
 wandb_project = 'nano-moe'
-#wandb_run_name='gpt2-124M-owt ' + time.strftime('%Y-%m-%d %H:%M:%S')
-wandb_run_name = 'gpt2-124M-owt-restart-0 2024-04-02 14:39:04'
+wandb_run_name ='gpt2-124M-moe-owt ' + time.strftime('%Y-%m-%d %H:%M:%S')
 
 # model/moe settings
-n_exp = 1
+n_exp = 8
+top_k = 2
+use_aux_loss = True
+aux_loss_weight = 0.01
+use_router_z_loss = True
+router_z_loss_weight = 0.001
+use_noisy_top_k = False
+train_capacity = 1.25
+eval_capacity = 2.0
+stride = 2
+use_switch_tfm_init = True
+router_use_full_prec = True
+
+# use smaller GPT model
+n_layer = 6
+n_head = 6
+n_embd = 384
 
 # these make the total batch size be ~0.5M
 # 12 batch size * 1024 block size * 5 gradaccum * 8 GPUs = 491,520
